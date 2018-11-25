ca4d1f4f0c8f03efe22bac6e9896014d62c594bf
Nicholas Burney <npb9925@louisiana.edu>
2018-11-25 13:54:31 -0600

    Game Build 1.4
    Fixed mispelling on Main Menu
    Fixed Lighting Error in FPPScene
    Removed bugged MegaFruit from TPP

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void Menu(string Menu)
     {
         SceneManager.LoadScene(Menu);
