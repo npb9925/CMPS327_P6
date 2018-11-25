7a9253f16ba0525c3208a298125f54efa2480579
Nicholas Burney <npb9925@louisiana.edu>
2018-11-25 04:17:12 -0600

    Added Win Screen, Music to Menues, and Comments to Code.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,16 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void Menu(string Menu)
     {
         SceneManager.LoadScene(Menu);
